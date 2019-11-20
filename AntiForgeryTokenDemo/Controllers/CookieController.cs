c8eb30e60102d3d682824c336b99f77991f67398
Matthew Dickson <mdickson972@gmail.com>
2019-11-20 14:14:44 +0000

    Updated initial

---
+++ AntiForgeryTokenDemo/Controllers/CookieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AntiForgeryTokenDemo.Controllers
+{
+    public class CookieController : Controller
+    {
+        [HttpGet]
+        public IActionResult Cookie() => View();
+
+        [HttpGet]
+        public IActionResult Generate() => View("Cookie");
+
+        /// <summary>
+        /// Receives an Antiforgery Token as part of the form data sent in the request.
+        /// Expecting property with ID: __RequestVerificationToken
+        /// </summary>
+        [HttpPost, AutoValidateAntiforgeryToken]
+        public string TestForm() => "Form submission successfull!";
+
+        /// <summary>
+        /// Receives an Antiforgery Token as part of the header sent in the request.
+        /// Expecting property with ID: X-CSRF-TOKEN
+        /// </summary>
+        [HttpPost, AutoValidateAntiforgeryToken]
+        public string TestApi() => "JSON submission successfull!";
+    }    
+}
---
+++ AntiForgeryTokenDemo/wwwroot/js/cookie.js
@@ -0,0 +1,78 @@
+﻿var antiforgeryTokenCookieIdentifier = 'CSRF-TOKEN';
+
+document.onload = setCookiePresenceText(antiforgeryTokenCookieIdentifier);
+
+function setCookiePresenceText(cname) {
+    $('#cookiePresenceHeader').text(getTokenFromCookie(cname) === ""
+        ? 'No Antiforgery token detected.'
+        : 'Antiforgery token has been generated and added as a Cookie.');
+}
+
+function getTokenFromCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$("#generate").click(function (e) {
+    e.preventDefault();
+    $.ajax({
+        url: "/Cookie/Generate/",
+        type: "POST",
+        success: function () {
+        },
+        error: function () {
+            alert('Post Unsuccessful');
+        }
+    });
+});
+
+$("#formTest").click(function (e) {
+    e.preventDefault();
+    $.ajax({
+        url: "/Cookie/TestForm/",
+        type: "POST",
+        data: {
+            '__RequestVerificationToken': getTokenFromCookie(antiforgeryTokenCookieIdentifier),
+            'Test': 'this is a test'
+        },
+        success: function (result) {
+            alert(result);
+        },
+        error: function () {
+            alert('Post Unsuccessful');
+        }
+    });
+});
+
+$("#jsonTest").click(function (e) {
+    e.preventDefault();
+
+    //var headers = {};
+    //headers['X-CSRF-TOKEN'] = getTokenFromCookie(antiforgeryTokenCookieIdentifier);
+
+    $.ajax({
+        url: "/Cookie/TestApi/",
+        type: "POST",
+        contentType: "application/json",
+        headers: {
+            'X-CSRF-TOKEN': getTokenFromCookie(antiforgeryTokenCookieIdentifier)
+        },
+        success: function (result) {
+            alert(result);
+        },
+        error: function () {
+            alert('Post Unsuccessful');
+        }
+    });
+});
